ba2e9569b8945619e8ff808141a251d7fe81894e
Dmitry Arsentiev <dmarsentev@gmail.com>
2020-07-11 00:27:37 +0300

    5-module_2-task

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,10 @@
 function toggleText() {
   // ваш код...
+  const btn = document.querySelector("button.toggle-text-button");
+  const div = document.querySelector("div#text");
+  btn.addEventListener('click', () => {
+    div.classList.toggle("invisible");
+    div.hidden = div.classList.contains("invisible");
+  });
+
 }
